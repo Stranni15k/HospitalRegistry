209f041c258a9306455eeaf4455fbee0fda13d18
Stranni15k <stranni19k@mail.ru>
2023-03-04 19:18:17 +0400

    RotationFile

---
+++ HospitalRegistry/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalRegistry
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Priveleges { get; set; }
+        public string Email { get; set; }
+    }
+}
